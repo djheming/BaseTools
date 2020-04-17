7dc161c2fce9032830e232ca40ef6f004e36cd14
djheming <douglas.hemingway@gmail.com>
2020-04-17 16:51:22 -0400

    Added functions: struct2argarray, timetostr, and progress_report.

--- BaseTools.m
+++ BaseTools.m
@@ -29,8 +29,16 @@ classdef BaseTools
             args = [];
             for i = 1 : 2 : length(argarray)-1
                 if ~isempty( argarray{i} )
-                    eval( [ 'args.' argarray{i} ' = argarray{i+1};' ] );
+                    args.(argarray{i}) = argarray{i+1};
                 end
+            end
+        end
+        function argarray = struct2argarray( args )
+            names = fieldnames( args );
+            argarray = cell( 1, 2*length(names) );
+            for i = 1 : length(names)
+                argarray{2*i-1} = names{i};
+                argarray{2*i} = args.(names{i});
             end
         end
         
@@ -39,6 +47,54 @@ classdef BaseTools
             MarkerSet = 'so^*dpvx><+.';
             MarkerSet = repmat( MarkerSet, 1, ceil(N/length(MarkerSet)) );
             MarkerSet = MarkerSet(1:N);
+        end
+        
+        % Report on how long something took.
+        function str = timetostr( t_elapsed )
+            if t_elapsed < 60
+                t_report = t_elapsed;
+                t_units = 'seconds';
+            elseif t_elapsed < BaseTools.sperhr
+                t_report = t_elapsed/60;
+                t_units = 'minutes';
+            elseif t_elapsed < BaseTools.sperday
+                t_report = t_elapsed/(BaseTools.sperhr);
+                t_units = 'hours';
+            elseif t_elapsed < BaseTools.speryr
+                t_report = t_elapsed/(BaseTools.sperday);
+                t_units = 'days';
+            elseif t_elapsed < BaseTools.speryr * 1e3
+                t_report = t_elapsed/(BaseTools.speryr);
+                t_units = 'years';
+            elseif t_elapsed < BaseTools.speryr * 1e6
+                t_report = t_elapsed/(BaseTools.speryr*1e3);
+                t_units = 'kyrs';
+            elseif t_elapsed < BaseTools.speryr * 1e9
+                t_report = t_elapsed/(BaseTools.speryr*1e6);
+                t_units = 'Myrs';
+            else
+                t_report = t_elapsed/(BaseTools.speryr*1e9);
+                t_units = 'Gyrs';
+            end
+            str = sprintf( '%.1f %s', t_report, t_units );
+        end
+        function progress_report( timername, k, N )
+            % Since <timername> timer was started, we have completed k of N
+            % steps. How are we doing and when will we be done? 
+            t_elapsed = toc(timername);
+            dt = (t_elapsed/k);
+            if t_elapsed < 90 % For the first 1.5 minutes.
+                interval = 10; % Every ten seconds.
+            elseif t_elapsed < 300 % For the first 5 minutes.
+                interval = 30; % Every 30 seconds.
+            else
+                interval = 300; % Every five minutes.
+            end
+            if t_elapsed > 5 && ( mod(k,round(interval/dt)) == 0 )
+                rt = dt*(N-k);
+                etc = now + datenum(0,0,0,0,0,rt);
+                fprintf( 'Completed %d/%d=%.1f%% in %s (%s each; ETC: %s).\n', k, N, 100*k/N, BaseTools.timetostr(t_elapsed), BaseTools.timetostr(dt), datestr(etc,13) );
+            end
         end
         
         % Obtain a sequence with nice round steps.
